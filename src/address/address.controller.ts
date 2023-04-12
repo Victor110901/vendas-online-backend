b9dc4fd7e27d2202d8c9740f10597f0b573c8f51
Victor1109 <victor.cs2001.vc@gmail.com>
2023-04-12 18:20:58 -0300

    feat: insert controller address

---
+++ src/address/address.controller.ts
@@ -0,0 +1,19 @@
+import { Param, Controller, Post, UsePipes, ValidationPipe, Body } from '@nestjs/common';
+import { AddressService } from './address.service';
+import { CreateAddressDto } from './dtos/createAddress.dto';
+import { AddressEntity } from './entities/address.entity';
+
+@Controller('address')
+export class AddressController {
+
+    constructor(private readonly addressService: AddressService) {}
+
+    @Post('/:userId')
+    @UsePipes(ValidationPipe)
+    async createAddress(
+        @Body() createAddressDto: CreateAddressDto,
+        @Param('userId') userId: number,    
+    ): Promise<AddressEntity> {
+        return this.addressService.createAddress(createAddressDto, userId);
+    }
+}
